b860d06c75d50e023d2604aff98bc6a3dd90048f
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2021-11-16 17:42:19 -0500

    Remove unused Junit5 dependency, upgrade gradle

--- src/test/java/helloworldapp/HelloWorldWorkflowTest.java
+++ src/test/java/helloworldapp/HelloWorldWorkflowTest.java
@@ -7,7 +7,6 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
 public class HelloWorldWorkflowTest {
