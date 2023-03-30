045f51d8a86dcb1179c5625c6bbbb7b327fd1320
Mason Egger <mason@masonegger.com>
2023-03-30 14:00:19 -0500

    updating pom.xml and changing variable to resemble java style

--- app/src/main/java/helloworldapp/Shared.java
+++ app/src/main/java/helloworldapp/Shared.java
@@ -4,7 +4,7 @@ package helloworldapp;
 public interface Shared {
 
     // Define the task queue name
-    final String HELLO_WORLD_TASK_QUEUE = "HELLO_WORLD_TASK_QUEUE";
+    final String HELLO_WORLD_TASK_QUEUE = "HelloWorldTaskQueue";
 
 }
 // @@@SNIPEND
